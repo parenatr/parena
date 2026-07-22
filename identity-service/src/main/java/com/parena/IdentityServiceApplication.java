1e8e072f5420347614d3e737ccb1f267a6382f2d
nursultanaslan <nursultanaslan1@gmail.com>
2026-07-22 22:35:02 +0300

    Initial commit

---
+++ identity-service/src/main/java/com/parena/IdentityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.parena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class IdentityServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdentityServiceApplication.class, args);
+    }
+
+}
