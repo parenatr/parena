3d70a7a7b2c9f6822e0e89fb52e8688241ec7d7b
nursultanaslan <nursultanaslan1@gmail.com>
2026-07-23 18:19:36 +0300

    feat: add user-service and configs

---
+++ bff-server/src/main/java/com/parena/bffserver/BffServerApplication.java
@@ -0,0 +1,13 @@
+package com.parena.bffserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class BffServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BffServerApplication.class, args);
+    }
+
+}
