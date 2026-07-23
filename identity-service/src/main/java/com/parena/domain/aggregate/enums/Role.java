a63d1102316b608a8ecedbdf6542f9a1c4058d02
nursultanaslan <nursultanaslan1@gmail.com>
2026-07-23 14:24:54 +0300

    feat: add gateway and identity service configurations

---
+++ identity-service/src/main/java/com/parena/domain/aggregate/enums/Role.java
@@ -0,0 +1,26 @@
+package com.parena.domain.aggregate.enums;
+
+public enum Role {
+    STANDARD_USER,
+    ADMIN;
+
+    //domain Enum Role ile calısıyor -> formatı ona gore ayarlar.
+    public static Role fromString(String roleStr) {
+        if (roleStr == null) {
+            return null;
+        }
+
+        String normalized = roleStr.toUpperCase();
+        try {
+            return Role.valueOf(normalized);
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException(String.format("Role '%s' is not a valid Role", normalized));
+        }
+
+    }
+
+    //keycloak apisinin istediği format.
+    public String toKeycloakRole() {
+        return this.name().toLowerCase();
+    }
+}
---
+++ identity-service/src/main/java/com/parena/domain/aggregate/root/User.java
@@ -0,0 +1,55 @@
+package com.parena.domain.aggregate.root;
+
+import com.parena.domain.aggregate.enums.Role;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.UUID;
+
+public class User {
+
+    private UUID userId;
+    private String email;
+    private String username;
+    private List<Role> roles;
+    private Instant createdAt;
+    private boolean emailVerified;
+
+    private User(UUID userId, String email, String username, List<Role> roles, Instant createdAt, boolean emailVerified) {
+        this.userId = userId;
+        this.email = email;
+        this.username = username;
+        this.roles = roles;
+        this.createdAt = createdAt;
+        this.emailVerified = emailVerified;
+    }
+
+    public static User create(UUID userId, String email, String username, List<Role> roles, Instant createdAt, boolean emailVerified) {
+        return new User(userId, email, username, roles, createdAt, emailVerified);
+    }
+
+
+    public UUID getUserId() {
+        return userId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public List<Role> getRoles() {
+        return roles;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public boolean isEmailVerified() {
+        return emailVerified;
+    }
+}
