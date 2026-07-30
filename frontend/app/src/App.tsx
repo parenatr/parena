9352f80592451789cbc0e62d1479d8211d4bea7a
nursultanaslan <nursultanaslan1@gmail.com>
2026-07-30 12:47:51 +0300

    deleted readme file and AppRouter move to router folder

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -1,6 +1,6 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-import { AppRouter } from "@/AppRouter";
+import { AppRouter } from "@/router/AppRouter";
 
 const queryClient = new QueryClient({
   defaultOptions: { queries: { retry: false, staleTime: 60_000 } },
